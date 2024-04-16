dcf989c2ed18455acdfe5615ac4c4a936acb5789
Pedro Augusto <pedroaugustodupas@hotmail.com>
2024-04-16 18:42:48 -0300

    bootstrap e materials

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(), provideAnimationsAsync()
   ]
 }).catch((err) =>
   console.error(err)

fcfc307032e143ad78d9ba4dac34c8bad4f50a44
Pedro Augusto <pedroaugustodupas@hotmail.com>
2024-05-07 20:35:24 -0300

    Trabalhos Interface

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -5,19 +5,22 @@ import { InputtextComponent } from '../components/inputs/inputtext/inputtext.com
 import { CheckboxComponent } from '../components/inputs/checkbox/checkbox.component';
 import { ButtonComponent } from '../components/buttons/button/button.component';
 import { JobsService } from './service/jobs.service';
+import { CdkDrag, CdkDragDrop, CdkDropList, CdkDropListGroup, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'jobs',
   standalone: true,
-  imports: [InputtextComponent, CheckboxComponent, ButtonComponent],
+  imports: [InputtextComponent, CheckboxComponent, ButtonComponent, CdkDropListGroup, CdkDropList, CdkDrag],
   templateUrl: './jobs.component.html',
   styleUrl: './jobs.component.css'
 })
 export class JobsComponent implements OnInit {
 
   trabalhoNovo: Jobs = new Jobs();
-  trabalhoUpdate: Jobs = new Jobs();
-  ListaTrabalho: Jobs[] = [];
+  trabalhoSelecionado: Jobs = new Jobs();
+  listaTrabalho: Jobs[] = [];
+  listaTrabalhoSelecionados: Jobs[] = [];
+
 
   constructor(
     private titleService: TitleService,
@@ -28,10 +31,11 @@ export class JobsComponent implements OnInit {
     setTimeout(() => {
       this.titleService.setPageTitle("Trabalhos");
     }, 10);
+    this.obterTrabalhos();
   }
 
   refresh() {
-    this.trabalhoUpdate = new Jobs();
+    this.trabalhoSelecionado = new Jobs();
     this.trabalhoNovo = new Jobs();
     this.obterTrabalhos();
   }
@@ -39,7 +43,7 @@ export class JobsComponent implements OnInit {
   obterTrabalhos(): void {
     this.trabalhoService.obterTrabalhos().subscribe({
       next: (result) => {
-        this.ListaTrabalho = result;
+        this.listaTrabalho = result;
       },
       error: (error) => {
         console.log('Erro ao carregar usuários:', error);
@@ -50,13 +54,37 @@ export class JobsComponent implements OnInit {
   adicionarTrabalho(): void {
     this.trabalhoService.criarTrabalhos(this.trabalhoNovo).subscribe({
       next: (result) => {
-        this.ListaTrabalho.push(result);
+        this.listaTrabalho.push(result);
         this.refresh()
       },
       error: (error) => {
         console.log('Erro ao adicionar usuário:', error);
       }
     });
+  }
+
+  dropSelecionado(event: CdkDragDrop<Jobs[]>, trabalhos: Jobs[]) {
+    if (this.listaTrabalhoSelecionados.length == 0) {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+  drop(event: CdkDragDrop<Jobs[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 
 }

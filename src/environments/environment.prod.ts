ae0d681a6d8f21c980638832dc0173967fdf4540
Pedro <pedroaugustodupas@hotmail.com>
2024-07-27 01:14:46 -0300

    Produção?

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://sua-api-de-producao.com' // Substitua pela URL real da sua API de produção
+  apiUrl: 'https://back-3nie.onrender.com' // Substitua pela URL real da sua API de produção
 };

20c8ff9fd3f5718909819372b1b642193007ecb1
Pedro <pedroaugustodupas@hotmail.com>
2024-11-03 17:10:18 -0300

    Home Responsiva

--- src/app/components/inputs/text/input-email/input-email.component.ts
+++ src/app/components/inputs/text/input-email/input-email.component.ts
@@ -1,8 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroupDirective, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 
 /** Error when invalid control is dirty, touched, or submitted. */
@@ -16,22 +16,48 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 @Component({
   selector: 'input-email',
   standalone: true,
-  imports: [MatFormFieldModule,MatInputModule,ReactiveFormsModule,CommonModule],
+  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule, CommonModule],
   templateUrl: './input-email.component.html',
-  styleUrl: './input-email.component.css'
+  styleUrls: ['./input-email.component.css']
 })
-export class InputEmailComponent {
-  @Input() label!: string;
-  @Input() placeholder!: string;
-  @Input() value!: any;
-  @Output() valueChange = new EventEmitter<any>();
+export class InputEmailComponent implements OnInit, OnChanges {
+  @Input() label: string = '';
+  @Input() placeholder: string = '';
+  @Input() value: string = '';
+  @Input() disabled: boolean = false;
+  @Output() valueChange = new EventEmitter<string>();
 
-  emailFormControl = new FormControl('', Validators.email);
+  emailFormControl: FormControl;
   matcher = new MyErrorStateMatcher();
 
-  onInputChange(event: any) {
-    const newValue = event.target.value;
-    this.value = newValue;
-    this.valueChange.emit(newValue);
+  constructor() {
+    this.emailFormControl = new FormControl({ value: this.value, disabled: this.disabled }, [
+      Validators.required,
+      Validators.email,
+    ]);
+  }
+
+  ngOnInit(): void {
+    this.inicializarFormControl();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['value'] && !changes['value'].firstChange) {
+      this.emailFormControl.setValue(this.value, { emitEvent: false });
+    }
+
+    if (changes['disabled']) {
+      if (this.disabled) {
+        this.emailFormControl.disable({ emitEvent: false });
+      } else {
+        this.emailFormControl.enable({ emitEvent: false });
+      }
+    }
+  }
+
+  private inicializarFormControl(): void {
+    this.emailFormControl.valueChanges.subscribe((newValue: string) => {
+      this.valueChange.emit(newValue);
+    });
   }
 }
